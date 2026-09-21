2d442c4c7a03d67efdb8bbed2866180399d1a774
Mohamed Dghar <dgharmohamed@gmail.com>
2026-09-21 13:37:23 +0000

    First Commit

--- Apprendre les techniques algorithmiques/FindMax.js
+++
@@ -1,9 +0,0 @@
-let Tab = [1,2,3,4,5,6,7,8,9,10];
-let Max = Tab[0];
-for (let i = 0 ; i < Tab.length; i++){
-    if (Tab[i] > Max){
-        Max = Tab[i];
-        console.log(Max);
-    }
-}
-console.log(Max);
